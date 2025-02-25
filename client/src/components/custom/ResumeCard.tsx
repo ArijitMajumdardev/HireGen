83cc8717a202ad23506a2bf0d4f399369cfadafe
arijit <arijitstoic@gmail.com>
2025-02-25 10:13:14 +0530

    Fixed the ResumeCard as links

--- client/src/components/custom/ResumeCard.tsx
+++ client/src/components/custom/ResumeCard.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { FileText } from "lucide-react";
 import { IResumeList } from '@/pages/Dashboard';
+import { Link } from 'react-router-dom';
 
 
 const formatDate = (isoString: string) => {
@@ -20,13 +21,14 @@ const ResumeCard = ({resumeList}:{resumeList:IResumeList[]}) => {
   return (
       <div className='mt-4 grid grid-cols-1 md:grid-cols-3 gap-6 '>
           {
-          resumeList.length>0 &&  resumeList.map((resume, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.2 }}
-            >
+        resumeList.length > 0 && resumeList.map((resume, index) => (
+          <motion.div
+          key={index}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: index * 0.2 }}
+          >
+              <Link to={`/resume/${resume.id}/edit`}>
               <Card>
                 <CardHeader className="flex flex-col items-center">
                   <FileText className={`w-10 h-10 text-blue-500`} />
@@ -36,7 +38,9 @@ const ResumeCard = ({resumeList}:{resumeList:IResumeList[]}) => {
                 Last edited: {formatDate(resume.createdAt)}
                 </CardContent>
               </Card>
+            </Link>
             </motion.div>
+
         ))}
     </div>
   )
