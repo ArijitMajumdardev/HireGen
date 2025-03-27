85417f976d9c4a75d6bb2062e566b072a24b4c5e
arijit <arijitstoic@gmail.com>
2025-03-27 10:07:11 +0530

    Added Types for Interview

---
+++ client/src/types/index.d.ts
@@ -0,0 +1,23 @@
+interface Interview {
+    id: string;
+    role: string;
+    level: string;
+    questions: string[];
+    techstack: string[];
+    createdAt: string;
+    userId: string;
+    type: string;
+    finalized: boolean;
+}
+  
+interface InterviewCardProps {
+  interviewId?: string;
+  userId?: string;
+  role: string;
+  type: string;
+  techstack: string[];
+  createdAt?: string;
+}
+interface TechIconProps {
+  techStack: string[];
+}
