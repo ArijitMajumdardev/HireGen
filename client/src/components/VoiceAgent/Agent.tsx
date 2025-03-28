dd4ff60f99da861ed9e706301dfef2b7031ed7a7
arijit <arijitstoic@gmail.com>
2025-03-29 00:50:09 +0530

    Implement interview generation page ui

--- client/src/components/VoiceAgent/Agent.tsx
+++ client/src/components/VoiceAgent/Agent.tsx
@@ -1,10 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
-const Agent = ({ userName,userId,type}:{userName:string,userId:string,type:string}) => {
+enum CallStatus {
+    INACTIVE = "INACTIVE",
+    CONNECTING = "CONNECTING",
+    ACTIVE = "ACTIVE",
+    FINISHED = "FINISHED",
+}
+  
+
+const Agent = ({
+  userName,
+  userId,
+  type,
+}: {
+  userName: string;
+  userId: string;
+  type: string;
+}) => {
     const [isSpeaking, setIsSpeaking] = useState(true);
+    const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+    const messages = ['what is your name?', 'My name is John Doe,nice to meet you!']
+    const lastMessage = messages[messages.length - 1];
   return (
+    <>
       <div className="call-view">
-           <div className="card-interviewer">
+        <div className="card-interviewer">
           <div className="avatar">
             <img
               src="/ai-avatar.png"
@@ -17,7 +39,7 @@ const Agent = ({ userName,userId,type}:{userName:string,userId:string,type:strin
           </div>
           <h3>AI Interviewer</h3>
         </div>
-           {/* User Profile Card */}
+        {/* User Profile Card */}
         <div className="card-border">
           <div className="card-content">
             <img
@@ -30,9 +52,41 @@ const Agent = ({ userName,userId,type}:{userName:string,userId:string,type:strin
             <h3>{userName}</h3>
           </div>
         </div>
-          
-    </div>
-  )
-}
+          </div>
+          {
+              messages.length > 0 && <div className="transcript-border">
+                  <div className="transcript">
+                      <p className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn opacity-100')}>
+                          {lastMessage}
+                      </p>
+                      
+                  </div>
+              </div>
+          }
+          <div className="w-full flex justify-center">
+              {callStatus !== CallStatus.ACTIVE ? (
+                 <button className="relative flex justify-center items-center btn-call " onClick={() => handleCall()}>
+                 <span
+                   className={cn(
+                     "absolute bg-[#49de50] h-[85%] w-[65%] animate-ping  rounded-full opacity-75",
+                     callStatus !== CallStatus.CONNECTING && "hidden"
+                   )}
+                 />
+     
+                 <span className="relative">
+                   {callStatus === CallStatus.INACTIVE || callStatus === CallStatus.FINISHED
+                     ? "Call"
+                     : ". . ."}
+                 </span>
+               </button>
+              ):(
+                <button className="btn-disconnect" onClick={() => handleDisconnect()}>
+                  End
+                </button>
+              )}
+          </div>
+    </>
+  );
+};
 
-export default Agent
+export default Agent;
