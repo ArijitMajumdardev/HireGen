aa4aeff4aafeb74765a9c89817b03cfd36921a36
arijit <arijitstoic@gmail.com>
2025-03-31 03:15:21 +0530

    Implemented AI voice agent for interview generation

--- client/src/components/VoiceAgent/Agent.tsx
+++ client/src/components/VoiceAgent/Agent.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
+import { vapi } from "@/lib/vapi.sdk";
+import { useNavigate } from "react-router-dom";
 
 enum CallStatus {
-    INACTIVE = "INACTIVE",
-    CONNECTING = "CONNECTING",
-    ACTIVE = "ACTIVE",
-    FINISHED = "FINISHED",
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
 }
-  
 
 const Agent = ({
   userName,
@@ -19,10 +20,83 @@ const Agent = ({
   userId: string;
   type: string;
 }) => {
-    const [isSpeaking, setIsSpeaking] = useState(true);
-    const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
-    const messages = ['what is your name?', 'My name is John Doe,nice to meet you!']
-    const lastMessage = messages[messages.length - 1];
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
+  const [lastMessage, setLastMessage] = useState<string>("");
+const navigate = useNavigate()
+  useEffect(() => {
+    const onCallStart = () => {
+      setCallStatus(CallStatus.ACTIVE);
+    };
+
+    const onCallEnd = () => {
+      setCallStatus(CallStatus.FINISHED);
+    };
+    const onMessage = (message: Message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        const newMessage = { role: message.role, content: message.transcript };
+        setMessages((prev) => [...prev, newMessage]);
+      }
+    };
+
+    const onSpeechStart = () => {
+      console.log("speech start");
+      setIsSpeaking(true);
+    };
+
+    const onSpeechEnd = () => {
+      console.log("speech end");
+      setIsSpeaking(false);
+    };
+
+    const onError = (error: Error) => {
+      console.log("Error:", error);
+    };
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    vapi.on("error", onError);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+      vapi.off("error", onError);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setLastMessage(messages[messages.length - 1].content);
+    }
+    if(callStatus===CallStatus.FINISHED) navigate('/')
+    },[messages, callStatus,type, userId])
+
+  //handle call connect
+  const handleCall = async () => {
+    setCallStatus(CallStatus.CONNECTING)
+
+    await vapi.start(import.meta.env.VITE_VAPI_WORKFLOW_ID, {
+      variableValues: {
+        username: userName,
+        userid:userId
+      }
+    })
+  }
+
+  //handle call disconnect
+  const handleDisconnect = () => {
+    setCallStatus(CallStatus.FINISHED);
+    vapi.stop();
+  };
+
+
   return (
     <>
       <div className="call-view">
@@ -52,39 +126,47 @@ const Agent = ({
             <h3>{userName}</h3>
           </div>
         </div>
-          </div>
-          {
-              messages.length > 0 && <div className="transcript-border">
-                  <div className="transcript">
-                      <p className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn opacity-100')}>
-                          {lastMessage}
-                      </p>
-                      
-                  </div>
-              </div>
-          }
-          <div className="w-full flex justify-center">
-              {callStatus !== CallStatus.ACTIVE ? (
-                 <button className="relative flex justify-center items-center btn-call " onClick={() => handleCall()}>
-                 <span
-                   className={cn(
-                     "absolute bg-[#49de50] h-[85%] w-[65%] animate-ping  rounded-full opacity-75",
-                     callStatus !== CallStatus.CONNECTING && "hidden"
-                   )}
-                 />
-     
-                 <span className="relative">
-                   {callStatus === CallStatus.INACTIVE || callStatus === CallStatus.FINISHED
-                     ? "Call"
-                     : ". . ."}
-                 </span>
-               </button>
-              ):(
-                <button className="btn-disconnect" onClick={() => handleDisconnect()}>
-                  End
-                </button>
+      </div>
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
               )}
+            >
+              {lastMessage}
+            </p>
           </div>
+        </div>
+      )}
+      <div className="w-full flex justify-center">
+        {callStatus !== CallStatus.ACTIVE ? (
+          <button
+            className="relative flex justify-center items-center btn-call "
+            onClick={() => handleCall()}
+          >
+            <span
+              className={cn(
+                "absolute bg-[#49de50] h-[85%] w-[65%] animate-ping  rounded-full opacity-75",
+                callStatus !== CallStatus.CONNECTING && "hidden"
+              )}
+            />
+
+            <span className="relative">
+              {callStatus === CallStatus.INACTIVE ||
+              callStatus === CallStatus.FINISHED
+                ? "Call"
+                : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect" onClick={() => handleDisconnect()}>
+            End
+          </button>
+        )}
+      </div>
     </>
   );
 };
