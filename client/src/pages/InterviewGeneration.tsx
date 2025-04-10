806330733f5c04a1b3e96e227ac4c7f9ca59e36d
arijit <arijitstoic@gmail.com>
2025-04-10 22:15:21 +0530

    UI changes : Interview Generation page

--- client/src/pages/InterviewGeneration.tsx
+++ client/src/pages/InterviewGeneration.tsx
@@ -1,22 +1,21 @@
 import Agent from '@/components/VoiceAgent/Agent';
 import { useAuthContext } from '@/context/AuthProvider';
-import React from 'react'
+import React from 'react';
 
 const InterviewGeneration = () => {
-    const {user} = useAuthContext()
-    return (
-        <>
-            <h3>Interview generation</h3>
-          
+  const { user } = useAuthContext();
 
-            <Agent
-        userName={user?.name!}
-        userId={user?.id!}
-        type="generate"
-      />
-    
-        </>
-      );
-}
+  return (
+    <div className="min-h-screen bg-[#05021E] px-6 py-10 text-white">
+      <h3 className="text-3xl font-bold mb-8 text-center">
+        Interview Generation
+      </h3>
 
-export default InterviewGeneration
+      <div className="max-w-5xl mx-auto bg-[#272533]/60 rounded-2xl shadow-md p-8">
+        <Agent userName={user?.name!} userId={user?.id!} type="generate" />
+      </div>
+    </div>
+  );
+};
+
+export default InterviewGeneration;
